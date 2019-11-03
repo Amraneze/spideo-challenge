e25e9131656eae0c67bc682f5f5f50e948c7fc6d
amranze <a.zeouayamran@gmail.com>
2019-11-03 19:46:47 +0100

    [Evo][Spideo][JavaDoc] Add JavaDoc
    Add JavaDoc for the controller and its service

--- src/main/java/tv/spideo/test/service/AuctionHouseService.java
+++ src/main/java/tv/spideo/test/service/AuctionHouseService.java
@@ -15,16 +15,40 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+/**
+ * The service which communicates with the controller and the repository (Database)
+ *
+ * @author Amrane Ait Zeouay
+ * @since 0.0.1
+ */
 @Service
 public class AuctionHouseService {
 
     private final AuctionHouseRepository auctionHouseRepository;
 
+    /**
+     * Create a new {@link AuctionHouseService} instance with the
+     * repository that will be injected to it.
+     *
+     * @param auctionHouseRepository an instance of {@link AuctionHouseRepository}
+     */
     @Autowired
     public AuctionHouseService(AuctionHouseRepository auctionHouseRepository) {
         this.auctionHouseRepository = auctionHouseRepository;
     }
 
+    /**
+     * A function that will create an auction house
+     * from a valid "AuctionHouse" model. It checks
+     * if the given name of the auction house is
+     * already used then we should throw an exception
+     * otherwise, save it in the database.
+     **
+     * @param auctionHouse the auction house that needs to be created
+     * @throws AuctionHouseAlreadyExistException if the auction house's name exist
+     * @throws GeneralException if there is an error occurred while saving the auction house
+     * @return a response entity of the same auction house but with an ID
+     */
     public AuctionHouse createAuctionHouse(AuctionHouse auctionHouse) {
         auctionHouseRepository.findAuctionHouseByName(auctionHouse.getName())
                 .ifPresent((existedAuctionHouse) -> { throw new AuctionHouseAlreadyExistException(); });
@@ -32,14 +56,32 @@ public class AuctionHouseService {
                 .orElseThrow(GeneralException::new);
     }
 
+    /**
+     * Get all auction houses in the actual database.
+     *
+     * @return an empty list or actual list of the auction houses
+     */
     public List<AuctionHouse> getAllAuctionHouses() {
         return auctionHouseRepository.findAllAuctionHouses();
     }
 
+    /**
+     * Get auctions by the id of their creator.
+     *
+     * @return an empty list or actual list of the auction houses
+     */
     public List<AuctionHouse> getAuctionHousesByCreatorId(String creatorId) {
         return auctionHouseRepository.findAllAuctionHousesByCreatorId(creatorId);
     }
 
+    /**
+     * Delete an auction house by id. It check if the auction house
+     * does exist before deleting it.
+     *
+     * @param auctionHouseId the auction house id that needs to be deleted
+     * @throws AuctionHouseNotFoundException if the auction house is not found in db
+     * @return A boolean, true if the auction house is deleted, false otherwise
+     */
     public boolean deleteAuctionHouse(String auctionHouseId) {
         AuctionHouse auctionHouse = auctionHouseRepository.findAuctionHouseById(auctionHouseId)
                 .orElseThrow(AuctionHouseNotFoundException::new);
@@ -47,11 +89,23 @@ public class AuctionHouseService {
                 .orElseThrow(AuctionHouseNotFoundException::new);
     }
 
-
+    /**
+     * Delete all auction houses to clear the db for test purpose.
+     */
     public void deleteAllAuctionHouse() {
         auctionHouseRepository.deleteAllAuctionHouses();
     }
 
+    /**
+     * Create an auction in a specific auction house, we should check
+     * if the auction house exist so we can create the auction inside
+     * of it otherwise we need to throw an exception.
+     *
+     * @param auctionHouseId the auction house id of the auction
+     * @param auction a valid {@link Auction} model
+     * @throws AuctionHouseNotFoundException if the auction house is not found in db
+     * @return The actual auction with a generated ID
+     */
     public Auction createAuction(String auctionHouseId, Auction auction) {
         return auctionHouseRepository.findAuctionHouseById(auctionHouseId)
                 .map((auctionHouse) -> {
@@ -63,20 +117,49 @@ public class AuctionHouseService {
                 .orElseThrow(AuctionHouseNotFoundException::new);
     }
 
+    /**
+     * Get all auctions of a specific auction house or throw
+     * the an exception if the auction house is not found
+     *
+     * @param auctionHouseId the auction house id that contains the auction
+     * @throws AuctionHouseNotFoundException if the auction house is not found in db
+     * @return An empty list or a list of auctions of that auction house
+     */
     public List<Auction> getAuctionsByAuctionHouseId(String auctionHouseId) {
         return auctionHouseRepository.findAuctionHouseById(auctionHouseId)
                 .map((auctionHouse) -> new ArrayList<>(auctionHouse.getAuctions().values()))
                 .orElseThrow(AuctionHouseNotFoundException::new);
     }
 
+    /**
+     * Delete an auction by id of an auction house or throw
+     * the an exception if the auction house or the auction
+     * is not found
+     *
+     * @param auctionHouseId the auction house id that we should to get the auctions
+     * @param auctionId the auction id that needs to be deleted
+     * @throws AuctionHouseNotFoundException if the auction house is not found in db
+     * @throws AuctionNotFoundException if the auction is not found in the list of auctions
+     * @return A boolean, true if the auction house is deleted, false otherwise
+     */
     public boolean deleteAuction(String auctionHouseId, String auctionId) {
         AuctionHouse auctionHouse = auctionHouseRepository.findAuctionHouseById(auctionHouseId)
                 .orElseThrow(AuctionHouseNotFoundException::new);
         Auction auction = Optional.ofNullable(auctionHouse.getAuctions().get(auctionId))
-                .orElseThrow(AuctionHouseNotFoundException::new);
+                .orElseThrow(AuctionNotFoundException::new);
         return auctionHouseRepository.deleteAuction(auctionHouse, auction).isPresent();
     }
 
+    /**
+     * Get auctions of an auction house by status or
+     * throw exception if the auction house is not
+     * found.
+     *
+     * @param auctionHouseId the auction house id that we should to get the auctions
+     * @param auctionStatus the status of the auction {@link Auction.AuctionStatus}
+     * @throws AuctionHouseNotFoundException if the auction house is not found in db
+     * @return An empty list or a list of filtered auctions with the given status
+     */
     public List<Auction> getAuctionsByStatus(String auctionHouseId, Auction.AuctionStatus auctionStatus) {
         return auctionHouseRepository.findAuctionHouseById(auctionHouseId)
                 .map((auctionHouse) -> auctionHouse.getAuctions().values()
@@ -87,6 +170,19 @@ public class AuctionHouseService {
                 .orElseThrow(AuctionHouseNotFoundException::new);
     }
 
+    /**
+     * Update an auction's status of an auction house or
+     * throw an exception if the auction is not finished,
+     * the auction or the auction house is not found.
+     *
+     * @param auctionHouseId the auction house id that we should to get the auctions
+     * @param auctionId the auction id that we should use to get the auction to update
+     * @param auctionStatus the new status of the auction
+     * @throws AuctionFinishedException if the auction already finished
+     * @throws AuctionNotFoundException if the auction is not found
+     * @throws AuctionHouseNotFoundException if the auction house is not found in db
+     * @return The auction with the updated status
+     */
     public Auction updateAuctionStatus(String auctionHouseId, String auctionId, Auction.AuctionStatus auctionStatus) {
         AuctionHouse auctionHouse = auctionHouseRepository.findAuctionHouseById(auctionHouseId)
                 .orElseThrow(AuctionHouseNotFoundException::new);
@@ -109,6 +205,22 @@ public class AuctionHouseService {
         return auction;
     }
 
+    /**
+     * Bid on an auction in an auction house or throw
+     * an exception if the auction did not start yet,
+     * the bidding's price is lower than the current
+     * auction's price, the auction or the auction
+     * house is not found.
+     *
+     * @param auctionHouseId the auction house id that we should to get the auctions
+     * @param auctionId the auction id that we should use to get the auction to bid on
+     * @param bidder a valid {@link AuctionBidder} that wants to bid
+     * @throws AuctionNotStartedException if the auction did not start yet
+     * @throws BiddingPriceLowException if the the bidder's price is lower than the current auction price
+     * @throws AuctionNotFoundException if the auction was not found in the list of the auctions
+     * @throws AuctionHouseNotFoundException if the auction house is not found in db
+     * @return The actual bidder with a generated id
+     */
     public AuctionBidder bidOnAuction(String auctionHouseId, String auctionId, AuctionBidder bidder) {
         return auctionHouseRepository.findAuctionHouseById(auctionHouseId)
                 .map((auctionHouse) -> {
@@ -130,6 +242,17 @@ public class AuctionHouseService {
                 .orElseThrow(AuctionHouseNotFoundException::new);
     }
 
+    /**
+     * Get all bidding of an auction in an auction house
+     * or throw an exception if the auction house or the
+     * auction is not found.
+     *
+     * @param auctionHouseId the auction house id that we should to get the auctions
+     * @param auctionId the auction id that we should use to get the auction
+     * @throws AuctionNotFoundException if the auction is not found in the list of auctions
+     * @throws AuctionHouseNotFoundException if the auction house is not found in db
+     * @return Map of id of bidders and prices that they used to bid
+     */
     public Map<String, Double> getAllBidding(String auctionHouseId, String auctionId) {
         AuctionHouse auctionHouse = auctionHouseRepository.findAuctionHouseById(auctionHouseId)
                 .orElseThrow(AuctionHouseNotFoundException::new);
@@ -146,6 +269,20 @@ public class AuctionHouseService {
                         Map.Entry::getValue));
     }
 
+    /**
+     * Get the auction's winner of an auction house or throw
+     * an exception if there was no bidding, the auction did
+     * not finish yet, the auction or auction house is not
+     * found.
+     *
+     * @param auctionHouseId the auction house id that we should to get the auctions
+     * @param auctionId the auction id that we should use to get the auction
+     * @throws AuctionNotFoundException if the auction is not found in the list of auctions
+     * @throws NoBiddingFoundException if there is no bidding, maybe we should throw GeneralException
+     * @throws AuctionNotFinishedException if the auction is not found in the list of auctions
+     * @throws AuctionHouseNotFoundException if the auction house is not found in db
+     * @return The winner of the auction {@link AuctionBidder}
+     */
     public AuctionBidder getAuctionWinner(String auctionHouseId, String auctionId) {
         Auction auction = auctionHouseRepository.findAuctionByHouseIdAndAuctionId(auctionHouseId, auctionId)
                 .filter((_auction) -> _auction.getStatus() == Auction.AuctionStatus.TERMINATED)
--- src/main/java/tv/spideo/test/web/controller/AuctionHouseController.java
+++ src/main/java/tv/spideo/test/web/controller/AuctionHouseController.java
@@ -15,6 +15,12 @@ import javax.validation.Valid;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * A REST controller which handles all HTTP requests starting with "/auctio/house/".
+ *
+ * @author Amrane Ait Zeouay
+ * @since 0.0.1
+ */
 @RestController
 @RequestMapping("/auction/house/")
 public class AuctionHouseController {
@@ -23,11 +29,28 @@ public class AuctionHouseController {
 
     private final AuctionHouseService auctionHouseService;
 
+    /**
+     * Create a new {@link AuctionHouseController} instance with the service injected to it.
+     *
+     * @param auctionHouseService an instance of {@link AuctionHouseService}
+     */
     @Autowired
     public AuctionHouseController(AuctionHouseService auctionHouseService) {
         this.auctionHouseService = auctionHouseService;
     }
 
+
+    /**
+     * A function that will create an auction house
+     * from a valid "AuctionHouse" model.
+     *
+     * <pre><code>Endpoint: POST /auction/house/</code></pre>
+     *
+     * @param auctionHouse the auction house that needs
+     * to be created
+     * @return a response entity of the same auction
+     * house but with an ID
+     */
     @PostMapping
     public ResponseEntity<AuctionHouse> createAuctionHouse(@Valid @RequestBody AuctionHouse auctionHouse) {
         logger.debug("Create a new Auction House {}", auctionHouse.toString());
@@ -36,8 +59,11 @@ public class AuctionHouseController {
     }
 
     /**
-     * Get all auction houses in the mocked db
-     * @return a list of the auction houses
+     * Get all auction houses in the actual database.
+     *
+     * <pre><code>Endpoint: GET /auction/house/</code></pre>
+     *
+     * @return an empty list or actual list of the auction houses
      */
     @GetMapping
     public ResponseEntity<List<AuctionHouse>> getAllAuctionHouses() {
@@ -46,6 +72,13 @@ public class AuctionHouseController {
                 .wrapResponse(auctionHouseService.getAllAuctionHouses());
     }
 
+    /**
+     * Get auctions by the id of their creator.
+     *
+     * <pre><code>Endpoint: GET /auction/house/:creatorId</code></pre>
+     *
+     * @return an empty list or actual list of the auction houses
+     */
     @GetMapping("creator/{creatorId}")
     public ResponseEntity<List<AuctionHouse>> getAuctionHousesByCreatorId(@PathVariable String creatorId) {
         logger.debug("Get all Auction Houses by creator id {}", creatorId);
@@ -59,9 +92,12 @@ public class AuctionHouseController {
      * to do it, only the user who created the auction house can
      * delete it, so in the deleteAuctionHouse we should check
      * if the person who wants to delete is the same as in
-     * Auction
-     * @param auctionHouseId the auction id
-     * @return ResponseEntity if everything went fine, otherwise an exception will be thrown
+     * Auction.
+     *
+     * <pre><code>Endpoint: DELETE /auction/house/:auctionHouseId</code></pre>
+     *
+     * @param auctionHouseId the auction house id that needs to be deleted
+     * @return ResponseEntity of a boolean if the auction house is deleted
      */
     @DeleteMapping("{auctionHouseId}")
     public ResponseEntity<Boolean> deleteAuctionHouse(@PathVariable String auctionHouseId) {
@@ -70,8 +106,18 @@ public class AuctionHouseController {
                 .wrapResponse(auctionHouseService.deleteAuctionHouse(auctionHouseId));
     }
 
-
-    @PostMapping(value = "{auctionHouseId}/create", consumes = {"application/json"})
+    /**
+     * Create an auction in a specific auction house, we should check
+     * if the auction house exist so we can create the auction inside
+     * of it otherwise we need to throw an exception.
+     *
+     * <pre><code>Endpoint: POST /auction/house/:auctionHouseId/create</code></pre>
+     *
+     * @param auctionHouseId the auction house id of the auction
+     * @param auction a valid {@link Auction} model
+     * @return ResponseEntity of the actual auction with a generated ID
+     */
+    @PostMapping("{auctionHouseId}/create")
     public ResponseEntity<Auction> createAuction(@PathVariable String auctionHouseId,
                                                  @Valid @RequestBody Auction auction) {
         logger.debug("Create a new Auction {}", auctionHouseId);
@@ -79,6 +125,14 @@ public class AuctionHouseController {
                 .wrapResponse(auctionHouseService.createAuction(auctionHouseId, auction));
     }
 
+    /**
+     * Get all auctions of a specific auction house.
+     *
+     * <pre><code>Endpoint: GET /auction/house/:auctionHouseId</code></pre>
+     *
+     * @param auctionHouseId the auction house id that contains the auction
+     * @return An empty list or a list of auctions of that auction house
+     */
     @GetMapping("{auctionHouseId}")
     public ResponseEntity<List<Auction>> getAuctionsByAuctionHouseId(@PathVariable String auctionHouseId) {
         logger.debug("Get all auctions of the auction house {}", auctionHouseId);
@@ -87,14 +141,13 @@ public class AuctionHouseController {
     }
 
     /**
-     * Delete an auction house by id, we should also check if the
-     * person who wants to delete the auction house has the rights
-     * to do it, only the user who created the auction house can
-     * delete it, so in the deleteAuctionHouse we should check
-     * if the person who wants to delete is the same as in
-     * Auction
-     * @param auctionHouseId the auction id
-     * @return ResponseEntity if everything went fine, otherwise an exception will be thrown
+     * Delete an auction by id of an auction house.
+     *
+     * <pre><code>Endpoint: DELETE /auction/house/:auctionHouseId/:auctionId</code></pre>
+     *
+     * @param auctionHouseId the auction house id that we should to get the auctions
+     * @param auctionId the auction id that needs to be deleted
+     * @return ResponseEntity of a boolean if the auction is deleted
      */
     @DeleteMapping("{auctionHouseId}/{auctionId}")
     public ResponseEntity<Boolean> deleteAuction(@PathVariable String auctionHouseId,
@@ -104,6 +157,15 @@ public class AuctionHouseController {
                 .wrapResponse( auctionHouseService.deleteAuction(auctionHouseId, auctionId));
     }
 
+    /**
+     * Get auctions of an auction house by status.
+     *
+     * <pre><code>Endpoint: GET /auction/house/:auctionHouseId/:status</code></pre>
+     *
+     * @param auctionHouseId the auction house id that we should to get the auctions
+     * @param status the status of the auction {@link Auction.AuctionStatus}
+     * @return An empty list or a list of filtered auctions with the given status
+     */
     @GetMapping("{auctionHouseId}/{status}")
     public ResponseEntity<List<Auction>> getAuctionsByStatus(@PathVariable String auctionHouseId,
                                                              @PathVariable Auction.AuctionStatus status) {
@@ -112,7 +174,17 @@ public class AuctionHouseController {
                 .wrapResponse(auctionHouseService.getAuctionsByStatus(auctionHouseId, status));
     }
 
-    @PutMapping(value = "{auctionHouseId}/{auctionId}/status/{status}", consumes = {"application/json"})
+    /**
+     * Update an auction's status of an auction house.
+     *
+     * <pre><code>Endpoint: PUT /auction/house/:auctionHouseId/:auctionId/:status</code></pre>
+     *
+     * @param auctionHouseId the auction house id that we should to get the auctions
+     * @param auctionId the auction id that we should use to get the auction to update
+     * @param status the new status of the auction
+     * @return The auction with the updated status
+     */
+    @PutMapping("{auctionHouseId}/{auctionId}/status/{status}")
     public ResponseEntity<Auction> updateAuctionStatus(@PathVariable String auctionHouseId,
                                                        @PathVariable String auctionId,
                                                        @PathVariable Auction.AuctionStatus status) {
@@ -121,7 +193,17 @@ public class AuctionHouseController {
                 .wrapResponse(auctionHouseService.updateAuctionStatus(auctionHouseId, auctionId, status));
     }
 
-    @PostMapping(value = "{auctionHouseId}/{auctionId}/bid", consumes = {"application/json"})
+    /**
+     * Bid on an auction in an auction house.
+     *
+     * <pre><code>Endpoint: POST /auction/house/:auctionHouseId/:auctionId/bid</code></pre>
+     *
+     * @param auctionHouseId the auction house id that we should to get the auctions
+     * @param auctionId the auction id that we should use to get the auction to bid on
+     * @param bidder a valid {@link AuctionBidder} that wants to bid
+     * @return The actual bidder with a generated id
+     */
+    @PostMapping("{auctionHouseId}/{auctionId}/bid")
     public ResponseEntity<AuctionBidder> bidOnAuction(@PathVariable String auctionHouseId,
                                                       @PathVariable String auctionId,
                                                       @Valid @RequestBody AuctionBidder bidder) {
@@ -130,6 +212,15 @@ public class AuctionHouseController {
                 .wrapResponse(auctionHouseService.bidOnAuction(auctionHouseId, auctionId, bidder));
     }
 
+    /**
+     * Get all bidding of an auction in an auction house.
+     *
+     * <pre><code>Endpoint: GET /auction/house/:auctionHouseId/:auctionId/bid</code></pre>
+     *
+     * @param auctionHouseId the auction house id that we should to get the auctions
+     * @param auctionId the auction id that we should use to get the auction
+     * @return Map of id of bidders and prices that they used to bid
+     */
     @GetMapping("{auctionHouseId}/{auctionId}/bid")
     public ResponseEntity<Map<String, Double>> getAllBiddingOfAuction(@PathVariable String auctionHouseId,
                                                                       @PathVariable String auctionId) {
@@ -138,6 +229,15 @@ public class AuctionHouseController {
                 .wrapResponse(auctionHouseService.getAllBidding(auctionHouseId, auctionId));
     }
 
+    /**
+     * Get the auction's winner of an auction house.
+     *
+     * <pre><code>Endpoint: GET /auction/house/:auctionHouseId/:auctionId/winner</code></pre>
+     *
+     * @param auctionHouseId the auction house id that we should to get the auctions
+     * @param auctionId the auction id that we should use to get the auction
+     * @return The winner of the auction {@link AuctionBidder}
+     */
     @GetMapping("{auctionHouseId}/{auctionId}/winner")
     public ResponseEntity<AuctionBidder> getAuctionWinner(@PathVariable String auctionHouseId,
                                                           @PathVariable String auctionId) {
